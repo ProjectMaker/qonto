6c11938adab8a05fe47b771c368185d4937a3f91
Thomas MICHELET <thomas@mobilispro.com>
2017-10-03 20:01:59 +0200

    [#1] missing remove pipe module :(

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { APP_SERVICES } from './services';
-import { PipeModule } from './pipes/pipes.module';
 import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   imports: [
     RouterModule,
-    PipeModule,
   ],
   declarations: [
     LayoutComponent
